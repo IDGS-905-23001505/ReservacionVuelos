34e2fb121c97d52cf93b46b8f707feaf00b4ab81
IDGS-805-23001505 <naomiteran755@gmail.com>
2026-07-16 16:02:00 -0600

    Actualizacion de codigo

--- app/src/main/java/com/example/reservacionvuelos/ui/theme/Color.kt
+++ app/src/main/java/com/example/reservacionvuelos/ui/theme/Color.kt
@@ -1,11 +1,18 @@
 package com.example.reservacionvuelos.ui.theme
 
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val HotelPrimary = Color(0xFFE94B77)
+val HotelPrimaryVariant = Color(0xFFC7325E)
+val HotelSecondary = Color(0xFF2D2D3A)
+val HotelBackground = Color(0xFFFDF8F9)
+val HotelCardBackground = Color.White
+val HotelTextMain = Color(0xFF222222)
+val HotelTextSub = Color(0xFF7E7A7C)
+val HotelInputBorder = Color(0xFFE5E5E5)
+val HotelErrorRed = Color(0xFFD32F2F)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val HotelPrimaryGradient = Brush.verticalGradient(
+    colors = listOf(HotelPrimary, HotelPrimaryVariant)
+)
